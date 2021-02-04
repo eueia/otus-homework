55966b29b9e267a7814c2d3205081675ee5eaea4
evgenia melnikova <evgenia.melnikova@iqoption.com>
2021-02-04 03:29:53 +0300

    update test

--- src/test/java/OtusProfileTest.java
+++ src/test/java/OtusProfileTest.java
@@ -1,15 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
-import java.net.URL;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import pages.OtusPage;
 import pages.OtusPersonalDataPage;
 
@@ -18,34 +16,13 @@ public class OtusProfileTest {
 
     private static final Logger logger = LogManager.getLogger(OtusProfileTest.class);
 
-//    @BeforeAll
-//    public static void setUp() {
-//        String browser = System.getProperty("browser_name").toLowerCase();
-//        WebDriverFactory wdf = new WebDriverFactory(browser);
-//        OtusPage.driver = wdf.createDriver(new DesiredCapabilities());
-//        logger.info("ChromeDriver is up");
-//    }
-//    @BeforeAll
-//    public static void setUp() {
-//        WebDriverManager.chromedriver().setup();
-//        OtusPage.driver = new ChromeDriver();
-//        logger.info("ChromeDriver is up");
-//    }
-
     @BeforeAll
     public static void setUp() {
         WebDriverManager.chromedriver().setup();
-        OtusPage.driver = new ChromeDriver();
-        String slenoidURL = "http://localhost:4444/wd/hub";
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setBrowserName("chrome");
-        caps.setVersion("86.0");
-        caps.setCapability("enableVNC", true);
-        caps.setCapability("screenResolution", "1280x1024");
-//        caps.setCapability("enableVideo", true);
-//        caps.setCapability("enableLog", true);
-
-        OtusPage.driver = new RemoteWebDriver(caps);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        OtusPage.driver = new ChromeDriver(options);
+        logger.info("ChromeDriver is up");
     }
 
     @Test
